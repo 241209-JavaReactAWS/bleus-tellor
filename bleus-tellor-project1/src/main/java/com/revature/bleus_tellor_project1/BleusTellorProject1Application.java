4f1092844a6ad712460ecb63504fdf9ed18da2b6
TamSell <tellorsam@gmail.com>
2024-12-11 13:11:00 -0500

    Added spring boot beginning framework

---
+++ bleus-tellor-project1/src/main/java/com/revature/bleus_tellor_project1/BleusTellorProject1Application.java
@@ -0,0 +1,13 @@
+package com.revature.bleus_tellor_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BleusTellorProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BleusTellorProject1Application.class, args);
+	}
+
+}
---
+++ bleus-tellor-project1/src/test/java/com/revature/bleus_tellor_project1/BleusTellorProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.bleus_tellor_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BleusTellorProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
